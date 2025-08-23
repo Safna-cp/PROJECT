3c2f1677f99ae360064582b70cbcf2f6fcaa51a5
Safna-cp <safnaclt123@gmail.com>
2025-08-23 13:52:21 +0530

    practise_panda.py

--- practise_panda.py
+++ practise_panda.py
@@ -14,9 +14,44 @@ import pandas as pd
 # dataframe head() function
 data ={
     'Name': ['alice','bob','charlie','david','eva','frank'],
-    'score': [85,90,78,92,88,76]
+    'score': [85,90,78.0,None,88,76],
+    'age': [20,21,19,None,20,23]
 }
 df=pd.DataFrame(data)
 print(df.head()) 
 print(df.head(3))  # first 3 rows
+
+
+# dataframe tail() function
+print(df.tail())  # last 5 rows
+
+
+# dataframe info() function
+print(df.info())
+
+
+# dataframe describe() function
+print(df.describe())
+
+# dataframe columns function(column names)
+print(df.columns) 
+
+# dataframe shape function (rows and columns)
+print(df.shape)
+
+
+# dataframe loc function (access rows and columns by labels)
+print(df.loc[2])  # row with index 2
+print(df.loc[:,['Name','score']]) 
+
+# dataframe iloc function (access rows and columns by integer position)
+print(df.iloc[0])  # row at position 2
+print(df.iloc[1,0])  # row 1, column 0
+print(df.iloc[:, 0:2])  # all rows, columns 0 and 1
+
+# dataframe isnull() function (check for missing values)
+print(df.isnull())
+
+# dataframe dropna() function (remove rows with missing values)
+print(df.dropna())  # remove rows with any missing values
 
