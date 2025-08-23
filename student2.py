ca7e54a32f274ad7ac07eb56015273014c78c85b
Fathima Safna <safnaclt123@gmail.com>
2025-08-23 10:37:03 +0530

    student2.py

---
+++ student2.py
@@ -0,0 +1,8 @@
+students = {
+    "safna": "100",
+    "amaya": "101",
+    "ashitha": "102"
+}
+name = input("Enter student name: ").strip().lower()
+student_id = students.get(name)
+print("id is:",student_id)
