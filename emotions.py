c92e72c48b53c971ae0402ae9f9a49eaef1696c7
Safna-cp <safnaclt123@gmail.com>
2025-08-23 10:05:11 +0530

    emotions.py

---
+++ emotions.py
@@ -0,0 +1,17 @@
+emotions={"happy": "😊", "sad": "😢", "angry": "😠", "love": "❤️", "surprised": "😲", "confused": "😕", "bored": "😐",
+         "excited": "🤩", "tired": "😴", "nervous": "😬"}
+print(emotions)
+
+# emotion = input("Enter an emotion: ").strip().lower()
+# if emotion in emotions:
+#     print(f"The emoji for {emotion} is {emotions[emotion]}")
+# else:
+#     print("Emotion not found.")
+
+
+emotion = input("Enter your message: ").strip().lower()
+words = emotion.split()
+converted = [emotions.get(word, word) for word in words]
+new_message = " ".join(converted)
+
+print(new_message)
