eea351dcdf27f619e56e4d74864434a28070f827
Fathima Safna <safnaclt123@gmail.com>
2025-09-09 16:36:54 +0530

    Add files via upload

---
+++ EDA_knnimputer.py
@@ -0,0 +1,10 @@
+from sklearn.impute import KNNImputer
+import pandas as pd
+data=pd.read_csv(r'C:\Users\pc42\Desktop\DATA SCIENCE\PROJECT\MISSING_DATASET_HANDLING.csv',encoding='latin1')
+print(data.isnull().sum())
+
+imputer=KNNImputer(n_neighbors=5)
+data_imputed=pd.DataFrame(imputer.fit_transform(data.select_dtypes(include=['float64','int64'])), columns=data.select_dtypes(include=['float64','int64']).columns)
+print(data_imputed.isnull().sum())
+
+
